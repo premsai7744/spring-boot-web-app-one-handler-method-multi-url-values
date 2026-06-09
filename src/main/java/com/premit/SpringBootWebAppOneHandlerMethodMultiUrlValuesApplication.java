969cf86466c6ce2a86df5dfa49296a30acf93ae7
mutha <mutha@192.168.1.35>
2026-06-09 15:05:02 +0530

    Initialize one handler method with multiple url values

---
+++ src/main/java/com/premit/SpringBootWebAppOneHandlerMethodMultiUrlValuesApplication.java
@@ -0,0 +1,13 @@
+package com.premit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppOneHandlerMethodMultiUrlValuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppOneHandlerMethodMultiUrlValuesApplication.class, args);
+	}
+
+}
